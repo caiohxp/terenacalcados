23d47c9dfe8c6ba4eda55f2908ae46dd387b66ba
Caio Cesar <59320997+caiohxp@users.noreply.github.com>
2023-01-16 15:13:54 -0300

    Upp ts app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './product';
+import { TipoProduto } from './tipo-produto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'terenacalcados';
+  title = {
+    firstName: "Terena",
+    secondName: "Calçados"
+  };
+  tipo: TipoProduto = TipoProduto.Bolsa;
+  filtro = false;
+  modalContent: Product = new Product("",TipoProduto.Bolsa);
+  products = [
+  new Product("bolsa", TipoProduto.Bolsa),new Product("cdourado", TipoProduto.Calcado),new Product("cmostarda",TipoProduto.Calcado),new Product("cpreto1", TipoProduto.Calcado),new Product("cpreto2",TipoProduto.Calcado)
+  ];
+  tamanhos = [35,36,37,38];
+  filtroProdutos(products: Product[]): Product[] {
+    return this.filtro? products.filter(p => p.tipo == this.tipo) : products;
+  }
+  semFiltro(){
+    this.filtro = false;
+  }
+  filtroCalcado(){
+    this.filtro = true;
+    this.tipo = TipoProduto.Calcado;
+  }
+  filtroBolsa(){
+    this.filtro = true;
+    this.tipo = TipoProduto.Bolsa;
+  }
+  modal(product: Product){
+    this.modalContent = product;
+  }
+  setTamanho(tamanho: number){
+    this.modalContent.tamanho = tamanho;
+    const divtamanho = document.querySelector(".tamanho")
+    divtamanho?.setAttribute("style", "display:block;")
+  }
 }
